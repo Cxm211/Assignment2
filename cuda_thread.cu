e845dae58a8262f5305f771881d4ceac19bb3ab3
Xuemeng <xuemengcai@gmail.com>
2021-10-23 22:21:00 +0800

    Update cuda_thread.cu

--- cuda_thread.cu
+++ cuda_thread.cu
@@ -101,7 +101,7 @@ __device__ int getNextState(const int *currWorld, const int *invaders, int nRows
             if (faction >= DEAD_FACTION)
             {
                 neighborCounts[faction]++;
-                printf("NEIGHBOR: %d", neighborCounts[faction]);
+               //printf("NEIGHBOR: %d", neighborCounts[faction]);
             }
         }
     }
@@ -121,7 +121,7 @@ __device__ int getNextState(const int *currWorld, const int *invaders, int nRows
         for (int faction = DEAD_FACTION + 1; faction < MAX_FACTIONS; faction++)
         {
             int count = neighborCounts[faction];
-
+            printf("COUNT: %d", count);
             if (isBirthable(count))
             {
                 newFaction = faction;
