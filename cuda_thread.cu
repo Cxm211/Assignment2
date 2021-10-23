fc67f31c3f38dae7154c2b0a02f3b995971d4fa8
Xuemeng <xuemengcai@gmail.com>
2021-10-23 22:04:58 +0800

    Update cuda_thread.cu

--- cuda_thread.cu
+++ cuda_thread.cu
@@ -167,7 +167,7 @@ __device__ int getNextState(const int *currWorld, const int *invaders, int nRows
 
 
 __global__ void execute( int * wholeNewWorld, const int *currWorld, const int *invaders, int nRows, int nCols ){
-    int tid = (threadIdx.x * blockDim.y + threadIdx.y ) + (blockDim.x * blockDim.y) * ( blockIdx.x * gridDim.y + blockIdx.y +  blockIdx.z * gridDim.x * gridDim.y );
+    int tid = (threadIdx.z * blockDim.y * blockDim.x + threadIdx.x * blockDim.y + threadIdx.y ) + (blockDim.x * blockDim.y * blockDim.z) * ( blockIdx.x * gridDim.y + blockIdx.y +  blockIdx.z * gridDim.x * gridDim.y );
     int num = gridDim.x * gridDim.y * gridDim.z * blockDim.x * blockDim.y * blockDim.z;
     printf("tid: %d ", tid);
     for (int row = 0; row < nRows; row++)
