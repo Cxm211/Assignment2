c2177f86dca75292a19aae60e73efb3db4ea8cf6
Xuemeng <xuemengcai@gmail.com>
2021-10-23 21:20:30 +0800

    Update cuda_thread.cu

--- cuda_thread.cu
+++ cuda_thread.cu
@@ -169,10 +169,7 @@ __device__ int getNextState(const int *currWorld, const int *invaders, int nRows
 __global__ void execute( int * wholeNewWorld, const int *currWorld, const int *invaders, int nRows, int nCols ){
     int tid = (threadIdx.x * blockDim.y + threadIdx.y ) + (blockDim.x * blockDim.y) * ( blockIdx.x * gridDim.y + blockIdx.y +  blockIdx.z * gridDim.x * gridDim.y );
     int num = gridDim.x * gridDim.y * gridDim.z * blockDim.x * blockDim.y * blockDim.z;
-# if __CUDA_ARCH__>=200
-    printf("%d \n", tid);
 
-#endif
     for (int row = 0; row < nRows; row++)
     {
         for (int col = 0; col < nCols; col++)
@@ -180,6 +177,10 @@ __global__ void execute( int * wholeNewWorld, const int *currWorld, const int *i
             int id = row * nCols + col;
             bool diedDueToFighting;
             if ( id % num == tid){
+# if __CUDA_ARCH__>=200
+                printf("%d \n", tid);
+
+#endif
                 int nextState = getNextState(currWorld, invaders, nRows, nCols, row, col, &diedDueToFighting);
                 setValueAt(wholeNewWorld, nRows, nCols, row, col, nextState);
                 if (diedDueToFighting){
