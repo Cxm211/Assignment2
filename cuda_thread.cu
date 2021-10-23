aec205216376ff3ff9b8a6505c9646d8a6feeccb
Xuemeng <xuemengcai@gmail.com>
2021-10-23 22:19:47 +0800

    Update cuda_thread.cu

--- cuda_thread.cu
+++ cuda_thread.cu
@@ -101,7 +101,7 @@ __device__ int getNextState(const int *currWorld, const int *invaders, int nRows
             if (faction >= DEAD_FACTION)
             {
                 neighborCounts[faction]++;
-                printf("NEIGHBOR: ", neighborCounts[faction]);
+                printf("NEIGHBOR: %d", neighborCounts[faction]);
             }
         }
     }
