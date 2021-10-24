8ab8b6738632b40393c4c4cff97f3c117b5c8cb9
James Nolan <vip17jamesnolan@gmail.com>
2021-10-24 13:36:50 +0800

    thanks god

--- cuda_thread.cu
+++ cuda_thread.cu
@@ -17,21 +17,22 @@
 // any integer value; changing this to a non-zero value may break the code
 #define DEAD_FACTION 0
 
+#ifndef MAX_THREAD_NUM
+#define MAX_THREAD_NUM 600000
+#endif
 /**
  * Specifies the number(s) of live neighbors of the same faction required for a dead cell to become alive.
  */
-__device__  int death[1000];
+__device__  int death[MAX_THREAD_NUM];
 
-__device__ bool isBirthable(int n)
-{
+__device__ bool isBirthable(int n) {
     return n == 3;
 }
 
 /**
  * Specifies the number(s) of live neighbors of the same faction required for a live cell to remain alive.
  */
-__device__ bool isSurvivable(int n)
-{
+__device__ bool isSurvivable(int n) {
     return n == 2 || n == 3;
 }
 
@@ -43,11 +44,8 @@ __device__ bool willFight(int n) {
 }
 
 
-
-void GlobalsetValueAt(int *grid, int nRows, int nCols, int row, int col, int val)
-{
-    if (row < 0 || row >= nRows || col < 0 || col >= nCols)
-    {
+void GlobalsetValueAt(int *grid, int nRows, int nCols, int row, int col, int val) {
+    if (row < 0 || row >= nRows || col < 0 || col >= nCols) {
         return;
     }
 
@@ -55,15 +53,14 @@ void GlobalsetValueAt(int *grid, int nRows, int nCols, int row, int col, int val
 }
 
 
-int GlobalgetValueAt(const int *grid, int nRows, int nCols, int row, int col)
-{
-    if (row < 0 || row >= nRows || col < 0 || col >= nCols)
-    {
+int GlobalgetValueAt(const int *grid, int nRows, int nCols, int row, int col) {
+    if (row < 0 || row >= nRows || col < 0 || col >= nCols) {
         return -1;
     }
 
     return *(grid + (row * nCols) + col);
 }
+
 /**
  * Computes and returns the next state of the cell specified by row and col based on currWorld and invaders. Sets *diedDueToFighting to
  * true if this cell should count towards the death toll due to fighting.
@@ -71,37 +68,33 @@ int GlobalgetValueAt(const int *grid, int nRows, int nCols, int row, int col)
  * invaders can be NULL if there are no invaders.
  */
 
-__device__ int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols, int row, int col, bool *diedDueToFighting)
-{
+__device__ int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols, int row, int col,
+                            bool *diedDueToFighting) {
     // we'll explicitly set if it was death due to fighting
     *diedDueToFighting = false;
     // faction of this cell
     int cellFaction = getValueAt(currWorld, nRows, nCols, row, col);
 
     // did someone just get landed on?
-    if (invaders != NULL && getValueAt(invaders, nRows, nCols, row, col) != DEAD_FACTION)
-    {
+    if (invaders != NULL && getValueAt(invaders, nRows, nCols, row, col) != DEAD_FACTION) {
         *diedDueToFighting = cellFaction != DEAD_FACTION;
         return getValueAt(invaders, nRows, nCols, row, col);
     }
 
     // tracks count of each faction adjacent to this cell
     int neighborCounts[MAX_FACTIONS];
-    for(int i = 0; i < MAX_FACTIONS; i++){
+    for (int i = 0; i < MAX_FACTIONS; i++) {
         neighborCounts[i] = 0;
     }
-   // memset(neighborCounts, 0, MAX_FACTIONS * sizeof(int));
+    // memset(neighborCounts, 0, MAX_FACTIONS * sizeof(int));
 
     // count neighbors (and self)
-    for (int dy = -1; dy <= 1; dy++)
-    {
-        for (int dx = -1; dx <= 1; dx++)
-        {
+    for (int dy = -1; dy <= 1; dy++) {
+        for (int dx = -1; dx <= 1; dx++) {
             int faction = getValueAt(currWorld, nRows, nCols, row + dy, col + dx);
-            if (faction >= DEAD_FACTION)
-            {
+            if (faction >= DEAD_FACTION) {
                 neighborCounts[faction]++;
-               //printf("NEIGHBOR: %d", neighborCounts[faction]);
+                //printf("NEIGHBOR: %d", neighborCounts[faction]);
             }
         }
     }
@@ -111,8 +104,7 @@ __device__ int getNextState(const int *currWorld, const int *invaders, int nRows
 //    for(int i = 0; i < MAX_FACTIONS; i++){
 //        printf("N: %d", neighborCounts[i]);
 //    }
-    if (cellFaction == DEAD_FACTION)
-    {
+    if (cellFaction == DEAD_FACTION) {
         // this is a dead cell; we need to see if a birth is possible:
         // need exactly 3 of a single faction; we don't care about other factions
 
@@ -120,20 +112,16 @@ __device__ int getNextState(const int *currWorld, const int *invaders, int nRows
         int newFaction = DEAD_FACTION;
 
         // start at 1 because we ignore dead neighbors
-        for (int faction = DEAD_FACTION + 1; faction < MAX_FACTIONS; faction++)
-        {
+        for (int faction = DEAD_FACTION + 1; faction < MAX_FACTIONS; faction++) {
             int count = neighborCounts[faction];
 //            printf("COUNT: %d", count);
-            if (isBirthable(count))
-            {
+            if (isBirthable(count)) {
                 newFaction = faction;
             }
         }
 
         return newFaction;
-    }
-    else
-    {
+    } else {
         /**
          * this is a live cell; we follow the usual rules:
          * Death (fighting): > 0 hostile neighbor
@@ -143,24 +131,20 @@ __device__ int getNextState(const int *currWorld, const int *invaders, int nRows
          */
 
         int hostileCount = 0;
-        for (int faction = DEAD_FACTION + 1; faction < MAX_FACTIONS; faction++)
-        {
-            if (faction == cellFaction)
-            {
+        for (int faction = DEAD_FACTION + 1; faction < MAX_FACTIONS; faction++) {
+            if (faction == cellFaction) {
                 continue;
             }
             hostileCount += neighborCounts[faction];
         }
 
-        if (willFight(hostileCount))
-        {
+        if (willFight(hostileCount)) {
             *diedDueToFighting = true;
             return DEAD_FACTION;
         }
 
         int friendlyCount = neighborCounts[cellFaction];
-        if (!isSurvivable(friendlyCount))
-        {
+        if (!isSurvivable(friendlyCount)) {
             return DEAD_FACTION;
         }
 
@@ -169,27 +153,33 @@ __device__ int getNextState(const int *currWorld, const int *invaders, int nRows
 }
 
 
+__global__ void
+execute(int *wholeNewWorld, const int *currWorld, const int *invaders, int nRows, int nCols, int eachThreadWork) {
+    int threadId = (threadIdx.z * blockDim.y * blockDim.x + threadIdx.x * blockDim.y + threadIdx.y) +
+                   (blockDim.x * blockDim.y * blockDim.z) *
+                   (blockIdx.x * gridDim.y + blockIdx.y + blockIdx.z * gridDim.x * gridDim.y);
+    int numTasksFinished = threadId * eachThreadWork;
+    int currentRowIndex = numTasksFinished / nCols; //<-Init Row Index
+    int currentColumnIndex = numTasksFinished % nCols; //<-Init Col Index
+    for (int i = 0; i < eachThreadWork; i++) {
+        //Check whether done
+        if (currentRowIndex >= nRows or currentColumnIndex >= nCols) return;
 
-__global__ void execute( int * wholeNewWorld, const int *currWorld, const int *invaders, int nRows, int nCols ){
-    int tid = (threadIdx.z * blockDim.y * blockDim.x + threadIdx.x * blockDim.y + threadIdx.y ) + (blockDim.x * blockDim.y * blockDim.z) * ( blockIdx.x * gridDim.y + blockIdx.y +  blockIdx.z * gridDim.x * gridDim.y );
-    int num = gridDim.x * gridDim.y * gridDim.z * blockDim.x * blockDim.y * blockDim.z;
+        /*** ALGO PART ***/
+        bool diedDueToFighting;
+        int nextState = getNextState(currWorld, invaders, nRows, nCols, currentRowIndex, currentColumnIndex,
+                                     &diedDueToFighting);
+        setValueAt(wholeNewWorld, nRows, nCols, currentRowIndex, currentColumnIndex, nextState);
+        if (diedDueToFighting) death[threadId]++;
+        diedDueToFighting = false;
+        /*** ALGO PART ***/
 
-    for (int row = 0; row < nRows; row++)
-    {
-        for (int col = 0; col < nCols; col++)
-        {
-            int id = row * nCols + col;
-            bool diedDueToFighting;
-            if ( id % num == tid){
-                //printf("tid: %d , id: %d\n", tid, id);
-                int nextState = getNextState(currWorld, invaders, nRows, nCols, row, col, &diedDueToFighting);
-                // printf("NEXT: %d\n", nextState);
-                setValueAt(wholeNewWorld, nRows, nCols, row, col, nextState);
-                if (diedDueToFighting){
-                    death[tid]++;
-                }
-                diedDueToFighting = false;
-            }
+        //Get next rowIndex and colIndex
+        if (currentColumnIndex == nCols - 1) {
+            currentRowIndex++;
+            currentColumnIndex = 0;
+        }else{
+            currentColumnIndex++;
         }
     }
 }
@@ -200,22 +190,22 @@ __global__ void execute( int * wholeNewWorld, const int *currWorld, const int *i
  * goi does not own startWorld, invasionTimes or invasionPlans and should not modify or attempt to free them.
  * nThreads is the number of threads to simulate with. It is ignored by the sequential implementation.
  */
-int goi_cuda(int GRID_X, int GRID_Y, int GRID_Z, int BLOCK_X, int BLOCK_Y, int BLOCK_Z, int nGenerations, const int *startWorld, int nRows, int nCols, int nInvasions, const int *invasionTimes, int **invasionPlans)
-{
+int goi_cuda(int GRID_X, int GRID_Y, int GRID_Z, int BLOCK_X, int BLOCK_Y, int BLOCK_Z, int nGenerations,
+             const int *startWorld, int nRows, int nCols, int nInvasions, const int *invasionTimes,
+             int **invasionPlans) {
     // death toll due to fighting
     int deathToll = 0;
     int num = GRID_X * GRID_Y * GRID_Z * BLOCK_X * BLOCK_Y * BLOCK_Z;
 
-    int start[1000];
-    int host_death[1000];
+    int start[MAX_THREAD_NUM];
+    int host_death[MAX_THREAD_NUM];
     for (int i = 0; i < num; i++) {
         start[i] = 0;
     }
 
     cudaError_t rc = cudaMemcpyToSymbol(death, &start, sizeof(start));
 
-    if (rc != cudaSuccess)
-    {
+    if (rc != cudaSuccess) {
         printf("Could not copy to device. Reason: %s\n", cudaGetErrorString(rc));
     }
 //    int death[num];
@@ -224,20 +214,17 @@ int goi_cuda(int GRID_X, int GRID_Y, int GRID_Z, int BLOCK_X, int BLOCK_Y, int B
     // init the world!
     // we make a copy because we do not own startWorld (and will perform free() on world)
     int *world = static_cast<int *>(malloc(sizeof(int) * nRows * nCols));
-    if (world == NULL)
-    {
+    if (world == NULL) {
         return -1;
     }
-    for (int row = 0; row < nRows; row++)
-    {
-        for (int col = 0; col < nCols; col++)
-        {
+    for (int row = 0; row < nRows; row++) {
+        for (int col = 0; col < nCols; col++) {
             GlobalsetValueAt(world, nRows, nCols, row, col, GlobalgetValueAt(startWorld, nRows, nCols, row, col));
         }
     }
 
     int *worldCuda;
-    cudaMalloc((void**)&worldCuda, sizeof(int) * nRows * nCols);
+    cudaMalloc((void **) &worldCuda, sizeof(int) * nRows * nCols);
     cudaMemcpy(worldCuda, world, sizeof(int) * nRows * nCols, cudaMemcpyHostToDevice);
 
 #if PRINT_GENERATIONS
@@ -251,27 +238,23 @@ int goi_cuda(int GRID_X, int GRID_Y, int GRID_Z, int BLOCK_X, int BLOCK_Y, int B
 
     // Begin simulating
     int invasionIndex = 0;
-    for (int i = 1; i <= nGenerations; i++)
-    {
+    for (int i = 1; i <= nGenerations; i++) {
         // is there an invasion this generation?
         int *inv = NULL;
         int *invCuda = NULL;
-        if (invasionIndex < nInvasions && i == invasionTimes[invasionIndex])
-        {
+        if (invasionIndex < nInvasions && i == invasionTimes[invasionIndex]) {
             // we make a copy because we do not own invasionPlans
             inv = static_cast<int *>(malloc(sizeof(int) * nRows * nCols));
-            cudaMalloc((void**)&invCuda, sizeof(int) * nRows * nCols);
-            if (inv == NULL)
-            {
+            cudaMalloc((void **) &invCuda, sizeof(int) * nRows * nCols);
+            if (inv == NULL) {
                 free(world);
                 cudaFree(worldCuda);
                 return -1;
             }
-            for (int row = 0; row < nRows; row++)
-            {
-                for (int col = 0; col < nCols; col++)
-                {
-                    GlobalsetValueAt(inv, nRows, nCols, row, col, GlobalgetValueAt(invasionPlans[invasionIndex], nRows, nCols, row, col));
+            for (int row = 0; row < nRows; row++) {
+                for (int col = 0; col < nCols; col++) {
+                    GlobalsetValueAt(inv, nRows, nCols, row, col,
+                                     GlobalgetValueAt(invasionPlans[invasionIndex], nRows, nCols, row, col));
                 }
             }
             cudaMemcpy(invCuda, inv, sizeof(int) * nRows * nCols, cudaMemcpyHostToDevice);
@@ -299,8 +282,8 @@ int goi_cuda(int GRID_X, int GRID_Y, int GRID_Z, int BLOCK_X, int BLOCK_Y, int B
 //        printf("HAHA\n");
 //        printWorld(world,  nRows,  nCols);
 //        printWorld(wholeNewWorld,  nRows,  nCols);
-        int *wholeNewWorldCuda  = NULL;
-        cudaMalloc((void**)&wholeNewWorldCuda, sizeof(int) * nRows * nCols);
+        int *wholeNewWorldCuda = NULL;
+        cudaMalloc((void **) &wholeNewWorldCuda, sizeof(int) * nRows * nCols);
 //        for (int row = 0; row < nRows; row++)
 //        {
 //            for (int col = 0; col < nCols; col++)
@@ -309,10 +292,10 @@ int goi_cuda(int GRID_X, int GRID_Y, int GRID_Z, int BLOCK_X, int BLOCK_Y, int B
 //            }
 //        }
 //        cudaMemcpy(wholeNewWorldCuda, wholeNewWorld, sizeof(int) * nRows * nCols, cudaMemcpyHostToDevice);
-        dim3 gridDim(GRID_X,GRID_Y,GRID_Z);
-        dim3 blockDim(BLOCK_X,BLOCK_Y, BLOCK_Z);
-
-        execute<<<gridDim, blockDim>>>(wholeNewWorldCuda, worldCuda, invCuda, nRows, nCols);
+        dim3 gridDim(GRID_X, GRID_Y, GRID_Z);
+        dim3 blockDim(BLOCK_X, BLOCK_Y, BLOCK_Z);
+        int eachThreadWork = (nRows * nCols - 1) / num + 1;
+        execute<<<gridDim, blockDim>>>(wholeNewWorldCuda, worldCuda, invCuda, nRows, nCols, eachThreadWork);
         cudaDeviceSynchronize();
 
 //     cudaMemcpy(wholeNewWorld, wholeNewWorldCuda, sizeof(int) * nRows * nCols, cudaMemcpyDeviceToHost);
@@ -333,8 +316,7 @@ int goi_cuda(int GRID_X, int GRID_Y, int GRID_Z, int BLOCK_X, int BLOCK_Y, int B
 //            }
 //        }
 
-        if (inv != NULL)
-        {
+        if (inv != NULL) {
             free(inv);
             cudaFree(invCuda);
         }
@@ -357,12 +339,11 @@ int goi_cuda(int GRID_X, int GRID_Y, int GRID_Z, int BLOCK_X, int BLOCK_Y, int B
 
     rc = cudaMemcpyFromSymbol(&host_death, death, sizeof(start));
 
-    if (rc != cudaSuccess)
-    {
+    if (rc != cudaSuccess) {
         printf("Could not copy from device. Reason: %s\n", cudaGetErrorString(rc));
     }
 
-    for (int i = 0; i < num; i++){
+    for (int i = 0; i < num; i++) {
         deathToll += host_death[i];
     }
 
